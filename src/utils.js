2cdcde07741c6d4ecbabd0dee5d3ffba1d0f06a8
tHBp <ramniksinghsodhi@gmail.com>
2016-09-26 21:05:41 +0530

    Improved extend function using MDN reference

--- src/utils.js
+++ src/utils.js
@@ -14,6 +14,17 @@ ko.utils = (function () {
                     target[prop] = source[prop];
                 }
             }
+            var descriptors = Object.keys(source).reduce(function(descriptors, key) {
+                descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
+                return descriptors;
+            }, {});
+            Object.getOwnPropertySymbols(source).forEach(function(sym) {
+                var descriptor = Object.getOwnPropertyDescriptor(source, sym);
+                if (descriptor.enumerable) {
+                    descriptors[sym] = descriptor;
+                }
+            });
+            Object.defineProperties(target, descriptors);
         }
         return target;
     }
